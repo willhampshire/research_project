207d264a04edae0e2a4680b68c386eacc304fa43
willhampshire <w.hampshire1@gmail.com>
2024-11-30 15:42:34 +0000

    5.1 detection

--- mode_detection.py
+++ mode_detection.py
@@ -23,7 +23,7 @@ from chars import greek, phys
 
 cwd = Path(os.getcwd())
 
-results_top_dir = cwd / "WS2_Grating_Eamonn" / "Results" / 'WS₂, SiO₂, Si 5'
+results_top_dir = cwd / "WS2_Grating_Eamonn" / "Results" / 'WS₂, SiO₂, Si 5.1'
 
 # results_dir = cwd / "WS2_Grating_Eamonn" / "Results" / 'WS₂, SiO₂, Si' / 't=18.0nm Λ=500nm FF=0.84 N=75' / 'data'
 # results_dir = cwd / "WS2_Grating_Eamonn" / "Results" / 'WS₂, SiO₂, Si 4' / 't=60.0nm Λ=350nm FF=0.55 N=75' / 'data'
@@ -54,7 +54,7 @@ def fit_simulation(results_dir):
 
     try:
         N_str = str(results_dir.parents[0]).rsplit('N=')[1]
-        N = int(N_str)
+        N = int(N_str) - 1
     except Exception as e:
         print(f"No N present in file name - {e}")
 
