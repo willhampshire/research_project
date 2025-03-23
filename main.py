afc493090dc230e2203234aea9983f8027b059b6
willhampshire <w.hampshire1@gmail.com>
2025-03-23 15:10:18 +0000

    large results

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-from pathlib import Path
-import os
-import matplotlib.pyplot as plt
-
-
-print("\nPlease run simulation script in WS2_Grating_Eamonn, \nor to view simulation images, image_browser.py.")
-
